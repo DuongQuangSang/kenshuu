b59bdc22a12975c5e1f030832612f97f1ddaa0bc
DuongQuangSang <95976361+DuongQuangSang@users.noreply.github.com>
2023-04-07 18:55:42 +0900

    Add files via upload
     5の階乗「i--」を使って修理します。

--- javakyoiku05/Q2.java
+++ javakyoiku05/Q2.java
@@ -8,10 +8,10 @@ public class Q2 {
 
 	public static void main(String[] args) {
 		int n = 1;
-		for(int i = 1; i <= 5; i++) {
+		for(int i = 5; i >= 1; i--) {
 			n *= i;
 		}
-		System.out.print(n);
+		System.out.print("5の階乗は"+ n);
 	}
 
 }
